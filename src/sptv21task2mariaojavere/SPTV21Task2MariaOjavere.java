a76fd46c31bc42fa1c02263c280064279a4d1a37
none <none@ee.ee>
2022-09-27 10:11:59 +0300

    commit2

---
+++ src/sptv21task2mariaojavere/SPTV21Task2MariaOjavere.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv21task2mariaojavere;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class SPTV21Task2MariaOjavere {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int n = input.nextInt();
+        System.out.println("Сумма цифр: " + sumDigits(n));
+    }
+    public static int sumDigits(int n){
+    int sum = 0;
+    while (n !=0){
+        sum += n % 10;
+        n /= 10;
+        }
+    return sum;
+    }
+}
+
